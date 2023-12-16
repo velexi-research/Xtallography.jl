3f1dfd943ddf418b63a05dd6224f8b8ca1470055
Kevin Chu <kevin@velexi.com>
2023-12-15 18:59:13 -0800

    Update docs/make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,25 +1,22 @@
 using XtallographyUtils
 using Documenter
 
-DocMeta.setdocmeta!(XtallographyUtils, :DocTestSetup, :(using XtallographyUtils); recursive=true)
+DocMeta.setdocmeta!(
+    XtallographyUtils, :DocTestSetup, :(using XtallographyUtils); recursive=true
+)
 
 makedocs(;
     modules=[XtallographyUtils],
     authors="Kevin Chu <kevin@velexi.com> and contributors",
-    repo="https://github.com/velexi-research/XtallographyUtils.jl/blob/{commit}{path}#{line}",
     sitename="XtallographyUtils.jl",
     format=Documenter.HTML(;
         prettyurls=get(ENV, "CI", "false") == "true",
         canonical="https://velexi-research.github.io/XtallographyUtils.jl",
+        repolink="https://github.com/velexi-research/XtallographyUtils.jl",
         edit_link="main",
         assets=String[],
     ),
-    pages=[
-        "Home" => "index.md",
-    ],
+    pages=["Home" => "index.md"],
 )
 
-deploydocs(;
-    repo="github.com/velexi-research/XtallographyUtils.jl",
-    devbranch="main",
-)
+deploydocs(; repo="github.com/velexi-research/XtallographyUtils.jl", devbranch="main")
