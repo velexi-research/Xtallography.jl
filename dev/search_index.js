7ef6bb58b987c357c21426814a61da4b8d1a6228
Documenter.jl <documenter@juliadocs.github.io>
2023-12-16 02:48:03 +0000

    build based on fd33203

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = XtallographyUtils","category":"page"},{"location":"#XtallographyUtils","page":"Home","title":"XtallographyUtils","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for XtallographyUtils.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [XtallographyUtils]","category":"page"}]
+}
