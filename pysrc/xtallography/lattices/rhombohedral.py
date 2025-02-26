4961be3fd88ab9002c3d231412dbdef4f5e6ce5f
Kevin Chu <kevin@velexi.com>
2025-02-25 17:30:27 -0800

    Fix license incantations.

--- pysrc/xtallography/lattices/rhombohedral.py
+++ pysrc/xtallography/lattices/rhombohedral.py
@@ -1,11 +1,16 @@
-# Copyright (c) 2024 Velexi Corporation
+#   Copyright 2025 Velexi Corporation
 #
-# Use of this software is governed by the Business Source License included
-# in the LICENSE file and at www.mariadb.com/bsl11.
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
 #
-# Change Date: Four years from the date the Licensed Work is published
+#       http://www.apache.org/licenses/LICENSE-2.0
 #
-# Change License: Mozilla Public License 2.0
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
 """
 The rhombohedral module defines classes and methods specific to rhombohedral lattices.
 """
