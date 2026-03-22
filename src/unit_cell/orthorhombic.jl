725b649723491d760c035ffafc7f456b90089f37
Kevin Chu <kevin@velexi.com>
2026-03-22 12:54:43 -0700

    Fix typo

--- src/unit_cell/orthorhombic.jl
+++ src/unit_cell/orthorhombic.jl
@@ -12,7 +12,7 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 """
-Orthorhombic  unit cell types and functions
+Orthorhombic unit cell types and functions
 """
 # --- Exports
 
