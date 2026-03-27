0bc773907246b71255ab5ac1f963cc4f0815acef
Kevin Chu <kevin@velexi.com>
2026-03-27 10:17:23 -0700

    Update unit tests for Lattice class

--- pysrc/tests/symmetry/test_lattice.py
+++ pysrc/tests/symmetry/test_lattice.py
@@ -23,8 +23,6 @@ import unittest
 import juliacall
 import pytest
 from xtallography.symmetry import Centering, Lattice, LatticeSystem
-from xtallography.symmetry import create_lattice, standardize_lattice
-from xtallography.symmetry import is_bravais_lattice
 
 # Local packages/modules
 
@@ -61,241 +59,79 @@ class test_xtallography_symmetry_lattice_Lattice(unittest.TestCase):
         """
         # --- Tests
 
-        # ------ Valid field values
-
-        # field values provided as LatticeSystem and Centering types
-        try:
-            Lattice(LatticeSystem.CUBIC, Centering.PRIMITIVE)
-            assert True
-        except Exception:
-            pytest.fail("Lattice() constructor raised unexpected error")
+        # field values provided as LatticeSystem and Centering objects
+        lattice = Lattice(LatticeSystem.CUBIC, Centering.PRIMITIVE)
+        print(lattice)
+        assert lattice.lattice_system == LatticeSystem.CUBIC
+        assert lattice.centering == Centering.PRIMITIVE
 
         # field values provided as strings
-        try:
-            Lattice("cubic", "primitive")
-            assert True
-        except Exception:
-            pytest.fail("Lattice() constructor raised unexpected error")
-
-        # ------ Invalid field values
-
-        # Nonsense `system` and `centering` values
-        #
-        # Note: Lattice() constructor doesn't validate values
-        try:
-            Lattice("abc", "def")
-            assert True
-        except Exception:
-            pytest.fail("Lattice() constructor raised unexpected error")
-
-        # `system` value has wrong type
-        #
-        # Note: Lattice() constructor doesn't validate value types
-        try:
-            Lattice(1, "def")
-            assert True
-        except Exception:
-            pytest.fail("Lattice() constructor raised unexpected error")
-
-        # `centering` value has wrong type
-        #
-        # Note: Lattice() constructor doesn't validate value types
-        try:
-            Lattice("abc", None)
-            assert True
-        except Exception:
-            pytest.fail("Lattice() constructor raised unexpected error")
-
-
-class test_xtallography_symmetry_lattice_functions(unittest.TestCase):
-    """
-    Test suite for lattice functions
-    """
-
-    # --- Fixtures
-
-    def setUp(self):
-        """
-        Prepare for test.
-        """
-        # Initialize JuliaCall
-        self.jl = juliacall.newmodule("PyXtallographyTest")
-        self.jl.seval("using Xtallography")
-
-    def tearDown(self):
-        """
-        Clean up after test.
-        """
-
-    # --- Tests
-
-    @staticmethod
-    def test_create_lattice():
-        """
-        Test `create_lattice()`.
-        """
-        # --- Tests
-
-        # Arguments valid and lowercase
-        lattice = create_lattice("cubic", "primitive")
-
+        lattice = Lattice("cubic", "primitive")
         assert lattice.lattice_system == LatticeSystem.CUBIC
         assert lattice.centering == Centering.PRIMITIVE
 
-        # Arguments valid but not lowercase
-        lattice = create_lattice("cuBIc", "pRIMitIVe")
-
+        # field values provided as strings but not lowercase
+        lattice = Lattice("cuBIc", "pRIMitIVe")
         assert lattice.lattice_system == LatticeSystem.CUBIC
         assert lattice.centering == Centering.PRIMITIVE
 
-        # Arguments provided as keyword arguments
-        lattice = create_lattice(
-            lattice_system="monoclinic",
-            centering="body",
-        )
+        # default centering argument
+        lattice = Lattice(lattice_system="monoclinic")
+        assert lattice.lattice_system == LatticeSystem.MONOCLINIC
+        assert lattice.centering == Centering.PRIMITIVE
 
+        # centering set as keyword argument
+        lattice = Lattice(lattice_system="monoclinic", centering=Centering.BODY)
         assert lattice.lattice_system == LatticeSystem.MONOCLINIC
         assert lattice.centering == Centering.BODY
 
-        # Arguments provided as LatticeSystem and Centering types
-        lattice = create_lattice(LatticeSystem.CUBIC, Centering.PRIMITIVE)
-
-        assert lattice.lattice_system == LatticeSystem.CUBIC
-        assert lattice.centering == Centering.PRIMITIVE
-
     @staticmethod
-    def test_create_lattice_invalid_args():
+    def test_init_invalid_arguments():
         """
-        Test argument checks for `create_lattice()`.
+        Test argument checks for `__init__()`
         """
         # --- Tests
 
-        # default arguments
+        # ------ lattice_system
+
+        # invalid `lattice_system` type
         with pytest.raises(ValueError) as exception_info:
-            create_lattice()
-
-        expected_error = "`lattice_system` must be a string. (lattice_system=None)"
-        assert expected_error in str(exception_info)
-
-        # valid `system`, default `centering`
-        with pytest.raises(ValueError) as exception_info:
-            create_lattice("cubic")
-
-        expected_error = "`centering` must be a string. (centering=None)"
-        assert expected_error in str(exception_info)
-
-        # default `system`, valid `centering` (via keyword argument)
-        with pytest.raises(ValueError) as exception_info:
-            create_lattice(centering="primitive")
-
-        expected_error = "`lattice_system` must be a string. (lattice_system=None)"
-        assert expected_error in str(exception_info)
-
-        # `system` has wrong type
-        with pytest.raises(ValueError) as exception_info:
-            create_lattice(1, "def")
-
-        expected_error = "`lattice_system` must be a string. (lattice_system=1)"
-        assert expected_error in str(exception_info)
-
-        # `centering` has wrong type
-        with pytest.raises(ValueError) as exception_info:
-            create_lattice("abc", None)
-
-        expected_error = "`centering` must be a string. (centering=None)"
-        assert expected_error in str(exception_info)
-
-        # Nonsense `system` and `centering` values
-        with pytest.raises(ValueError) as exception_info:
-            create_lattice("abc", "def")
+            Lattice(1)
 
         expected_error = (
-            f"`lattice_system` must be one of "
-            f"{LatticeSystem.values()}. "
+            "`lattice_system` must be a LatticeSystem object or a string. "
+            "(lattice_system=1)"
+        )
+        assert expected_error in str(exception_info)
+
+        # invalid `lattice_system` value
+        with pytest.raises(ValueError) as exception_info:
+            Lattice("abc")
+
+        expected_error = (
+            f"`lattice_system` must be one of {LatticeSystem.values()}. "
             "(lattice_system='abc')"
         )
         assert expected_error in str(exception_info)
 
-        # Nonsense `centering` value
+        # ------ centering
+
+        # invalid `centering` type
         with pytest.raises(ValueError) as exception_info:
-            create_lattice("hexagonal", "def")
+            Lattice(LatticeSystem.TETRAGONAL, centering=1)
 
         expected_error = (
-            "`centering` must be one of " f"{Centering.values()}. " "(centering='def')"
+            "`centering` must be a Centering object or a string. (centering=1)"
         )
         assert expected_error in str(exception_info)
 
-    @staticmethod
-    def test_standardize_lattice():
-        """
-        Test `standardize_lattice()`.
-        """
-        # --- Tests
-
-        # Field values are valid and already standardized
-        lattice = Lattice("cubic", "primitive")
-
-        lattice = standardize_lattice(lattice)
-
-        assert isinstance(lattice.lattice_system, LatticeSystem)
-        assert lattice.lattice_system == LatticeSystem.CUBIC
-
-        assert isinstance(lattice.centering, Centering)
-        assert lattice.centering == Centering.PRIMITIVE
-
-        # Field values are valid but not standardized
-        lattice = Lattice("cuBIc", "pRIMitIVe")
-
-        lattice = standardize_lattice(lattice)
-
-        assert isinstance(lattice.lattice_system, LatticeSystem)
-        assert lattice.lattice_system == LatticeSystem.CUBIC
-
-        assert isinstance(lattice.centering, Centering)
-        assert lattice.centering == Centering.PRIMITIVE
-
-    @staticmethod
-    def test_standardize_lattice_invalid_args():
-        """
-        Test argument checks for `standardize_lattice()`.
-        """
-        # --- Tests
-
-        # `system` has wrong type
+        # invalid `centering` value
+        # invalid `lattice_system` value
         with pytest.raises(ValueError) as exception_info:
-            standardize_lattice(Lattice(1, "def"))
+            Lattice("rhombohedral", "def")
 
         expected_error = (
-            "`lattice.lattice_system` must be a string. (lattice.lattice_system=1)"
-        )
-        assert expected_error in str(exception_info)
-
-        # `centering` has wrong type
-        with pytest.raises(ValueError) as exception_info:
-            standardize_lattice(Lattice("abc", None))
-
-        expected_error = (
-            "`lattice.centering` must be a string. (lattice.centering=None)"
-        )
-        assert expected_error in str(exception_info)
-
-        # Nonsense `system` and `centering` values
-        with pytest.raises(ValueError) as exception_info:
-            standardize_lattice(Lattice("abc", "def"))
-
-        expected_error = (
-            "`lattice_system` must be one of "
-            f"{LatticeSystem.values()}. "
-            "(lattice_system='abc')"
-        )
-        assert expected_error in str(exception_info)
-
-        # Nonsense `centering` value
-        with pytest.raises(ValueError) as exception_info:
-            standardize_lattice(Lattice("hexagonal", "def"))
-
-        expected_error = (
-            "`centering` must be one of " f"{Centering.values()}. " "(centering='def')"
+            "`centering` must be one of {Centering.values()}. (centering='def')"
         )
         assert expected_error in str(exception_info)
 
@@ -310,162 +146,124 @@ class test_xtallography_symmetry_lattice_functions(unittest.TestCase):
 
         # (triclinic, primitive)
         lattice = Lattice("triclinic", "primitive")
+        assert lattice.is_bravais_lattice()
 
-        assert is_bravais_lattice(lattice)
+        # (triclinic, body)
+        lattice = Lattice(LatticeSystem.TRICLINIC, Centering.BODY)
+        assert not lattice.is_bravais_lattice()
+
+        # (triclinic, base)
+        lattice = Lattice(LatticeSystem.TRICLINIC, Centering.BASE)
+        assert not lattice.is_bravais_lattice()
+
+        # (triclinic, face)
+        lattice = Lattice(LatticeSystem.TRICLINIC, Centering.FACE)
+        assert not lattice.is_bravais_lattice()
 
         # --------- monoclinic
 
         # (monoclinic, primitive)
         lattice = Lattice("monoclinic", "primitive")
-
-        assert is_bravais_lattice(lattice)
+        assert lattice.is_bravais_lattice()
 
         # (monoclinic, body)
         lattice = Lattice("monoclinic", "body")
-
-        assert is_bravais_lattice(lattice)
+        assert lattice.is_bravais_lattice()
 
         # (monoclinic, base)
         lattice = Lattice("monoclinic", "base")
+        assert lattice.is_bravais_lattice()
 
-        assert is_bravais_lattice(lattice)
+        # (monoclinic, face)
+        lattice = Lattice(LatticeSystem.MONOCLINIC, Centering.FACE)
+        assert not lattice.is_bravais_lattice()
 
         # --------- orthorhombic
 
         # (orthorhombic, primitive)
         lattice = Lattice("orthorhombic", Centering.PRIMITIVE)
-
-        assert is_bravais_lattice(lattice)
+        assert lattice.is_bravais_lattice()
 
         # (orthorhombic, body)
         lattice = Lattice(LatticeSystem.ORTHORHOMBIC, "body")
-
-        assert is_bravais_lattice(lattice)
+        assert lattice.is_bravais_lattice()
 
         # (orthorhombic, base)
         lattice = Lattice(LatticeSystem.ORTHORHOMBIC, Centering.BASE)
-
-        assert is_bravais_lattice(lattice)
+        assert lattice.is_bravais_lattice()
 
         # (orthorhombic, face)
         lattice = Lattice("orthorhombic", "face")
-
-        assert is_bravais_lattice(lattice)
+        assert lattice.is_bravais_lattice()
 
         # --------- tetragonal
 
         # (tetragonal, primitive)
         lattice = Lattice("tetragonal", Centering.PRIMITIVE)
-
-        assert is_bravais_lattice(lattice)
+        assert lattice.is_bravais_lattice()
 
         # (tetragonal, body)
         lattice = Lattice(LatticeSystem.TETRAGONAL, Centering.BODY)
+        assert lattice.is_bravais_lattice()
 
-        assert is_bravais_lattice(lattice)
+        # (tetragonal, base)
+        lattice = Lattice(LatticeSystem.TETRAGONAL, Centering.BASE)
+        assert not lattice.is_bravais_lattice()
+
+        # (tetragonal, face)
+        lattice = Lattice(LatticeSystem.TETRAGONAL, Centering.FACE)
+        assert not lattice.is_bravais_lattice()
 
         # --------- rhomobohedral
 
         # (rhombohedral, primitive)
         lattice = Lattice("rhombohedral", Centering.PRIMITIVE)
+        assert lattice.is_bravais_lattice()
 
-        assert is_bravais_lattice(lattice)
+        # (rhombohedral, body)
+        lattice = Lattice(LatticeSystem.RHOMBOHEDRAL, Centering.BODY)
+        assert not lattice.is_bravais_lattice()
+
+        # (rhombohedral, base)
+        lattice = Lattice(LatticeSystem.RHOMBOHEDRAL, Centering.BASE)
+        assert not lattice.is_bravais_lattice()
+
+        # (rhombohedral, face)
+        lattice = Lattice(LatticeSystem.RHOMBOHEDRAL, Centering.FACE)
+        assert not lattice.is_bravais_lattice()
 
         # --------- hexagonal
 
         # (hexagonal, primitive)
         lattice = Lattice("hexagonal", "primitive")
+        assert lattice.is_bravais_lattice()
 
-        assert is_bravais_lattice(lattice)
+        # (hexagonal, body)
+        lattice = Lattice(LatticeSystem.HEXAGONAL, Centering.BODY)
+        assert not lattice.is_bravais_lattice()
+
+        # (hexagonal, base)
+        lattice = Lattice(LatticeSystem.HEXAGONAL, Centering.BASE)
+        assert not lattice.is_bravais_lattice()
+
+        # (hexagonal, face)
+        lattice = Lattice(LatticeSystem.HEXAGONAL, Centering.FACE)
+        assert not lattice.is_bravais_lattice()
 
         # --------- cubic
 
         # (cubic, primitive)
         lattice = Lattice("cubic", "primitive")
-
-        assert is_bravais_lattice(lattice)
+        assert lattice.is_bravais_lattice()
 
         # (cubic, body)
         lattice = Lattice(LatticeSystem.CUBIC, "body")
-
-        assert is_bravais_lattice(lattice)
-
-        # (cubic, face)
-        lattice = Lattice("cubic", "face")
-
-        assert is_bravais_lattice(lattice)
-
-    @staticmethod
-    def test_is_bravais_lattice_invalid_lattice():
-        """
-        Test `is_bravais_lattice()`.
-        """
-        # --- Tests
-
-        # --------- triclinic
-
-        # (triclinic, body)
-        lattice = Lattice(LatticeSystem.TRICLINIC, Centering.BODY)
-        assert not is_bravais_lattice(lattice)
-
-        # (triclinic, base)
-        lattice = Lattice(LatticeSystem.TRICLINIC, Centering.BASE)
-        assert not is_bravais_lattice(lattice)
-
-        # (triclinic, face)
-        lattice = Lattice(LatticeSystem.TRICLINIC, Centering.FACE)
-        assert not is_bravais_lattice(lattice)
-
-        # --------- monoclinic
-
-        # (monoclinic, face)
-        lattice = Lattice(LatticeSystem.MONOCLINIC, Centering.FACE)
-        assert not is_bravais_lattice(lattice)
-
-        # --------- orthorhombic
-
-        # all centerings are valid
-
-        # --------- tetragonal
-
-        # (tetragonal, base)
-        lattice = Lattice(LatticeSystem.TETRAGONAL, Centering.BASE)
-        assert not is_bravais_lattice(lattice)
-
-        # (tetragonal, face)
-        lattice = Lattice(LatticeSystem.TETRAGONAL, Centering.FACE)
-        assert not is_bravais_lattice(lattice)
-
-        # --------- rhomobohedral
-
-        # (rhombohedral, body)
-        lattice = Lattice(LatticeSystem.RHOMBOHEDRAL, Centering.BODY)
-        assert not is_bravais_lattice(lattice)
-
-        # (rhombohedral, base)
-        lattice = Lattice(LatticeSystem.RHOMBOHEDRAL, Centering.BASE)
-        assert not is_bravais_lattice(lattice)
-
-        # (rhombohedral, face)
-        lattice = Lattice(LatticeSystem.RHOMBOHEDRAL, Centering.FACE)
-        assert not is_bravais_lattice(lattice)
-
-        # --------- hexagonal
-
-        # (hexagonal, body)
-        lattice = Lattice(LatticeSystem.HEXAGONAL, Centering.BODY)
-        assert not is_bravais_lattice(lattice)
-
-        # (hexagonal, base)
-        lattice = Lattice(LatticeSystem.HEXAGONAL, Centering.BASE)
-        assert not is_bravais_lattice(lattice)
-
-        # (hexagonal, face)
-        lattice = Lattice(LatticeSystem.HEXAGONAL, Centering.FACE)
-        assert not is_bravais_lattice(lattice)
-
-        # --------- cubic
+        assert lattice.is_bravais_lattice()
 
         # (cubic, base)
         lattice = Lattice(LatticeSystem.CUBIC, Centering.BASE)
-        assert not is_bravais_lattice(lattice)
+        assert not lattice.is_bravais_lattice()
+
+        # (cubic, face)
+        lattice = Lattice("cubic", "face")
+        assert lattice.is_bravais_lattice()
