c697299bcc5d73affd3f435f88250278062f796d
Kevin Chu <kevin@velexi.com>
2026-05-29 18:32:21 -0700

    Polish docstrings

--- pysrc/xtallography/symmetry/lattice_system.py
+++ pysrc/xtallography/symmetry/lattice_system.py
@@ -12,7 +12,7 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 """
-Lattice system class and utility functions
+Lattice system class
 """
 # --- Imports
 
